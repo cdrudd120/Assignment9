128aee48facb06856ab26a2b953a0ea8e2e17708
tobiasmache <47459647+tobiasmache@users.noreply.github.com>
2021-04-07 17:48:43 -0600

    Add files via upload

--- HashTable.java
+++ HashTable.java
@@ -89,33 +89,84 @@ public class HashTable<K, V> implements Map<K, V>{
 
 	@Override
 	public V put(K key, V value) {
+		if(calculateLoadFactor()>5) {
+			reHash();
+		}
 		int index = Math.abs(key.hashCode()) % table.size();
-		V temp;
+		MapEntry<K, V> newEntry = new MapEntry<K, V>(key, value);
+		MapEntry<K, V> oldEntry = new MapEntry<K, V>(key, value);
+		//if key already exists in map
 		if(containsKey(key) == true) {
-			temp = this.get(key);
+			V oldValue = this.get(key);
+			//loops through LinkedList
 			for(int i = 0; i < table.get(index).size(); i++) {
-				if(table.get(index).get(i).getKey() == key) {
-					V valueInMap = table.get(index).get(i).getValue();
-					valueInMap = value;
-					return temp;
+				//changes oldEntry to always be current MapEntry
+				oldEntry=table.get(index).get(i);
+				if (oldEntry.getKey().equals(newEntry.getKey())) {
+					oldEntry=newEntry;
 				}
+				size++;
+				return oldValue;
 			}
 		}
-		MapEntry<K, V> newEntry = new MapEntry<K, V>(key, value);
-		
-		
+		else {
+			table.get(index).add(newEntry);
+			size++;
+		}
 		return null;
 	}
 
 	@Override
 	public V remove(K key) {
-		// TODO Auto-generated method stub
-		return null;
+		int index = Math.abs(key.hashCode()) % table.size();
+		MapEntry<K, V> currentEntry = new MapEntry<K, V>(null, null);
+		if(containsKey(key) == true) {
+			V oldValue = this.get(key);
+			findAndRemoveLinkedList(key);
+			return oldValue;
+			}
+		else {
+			findAndRemoveLinkedList(key);
+			return null;
+		}
+	}
+	
+	//helper method
+	private void findAndRemoveLinkedList(K key) {
+		int index = Math.abs(key.hashCode()) % table.size();
+		MapEntry<K, V> currentEntry = new MapEntry<K, V>(null, null);
+		for(int i = 0; i < table.get(index).size(); i++) {
+			currentEntry=table.get(index).get(i);
+			if (currentEntry.getKey().equals(currentEntry.getKey())) {
+				//once found removes at index i in linked list
+				table.get(index).remove(i);
+			}
+			size--;
+		}
 	}
 
 	@Override
 	public int size() {
 		return size;
+	}
+	
+	//helper method to calculate lamda
+	private double calculateLoadFactor() {
+		double totalListLength=0;
+		for(int i=0; i<table.size(); i++) {
+			totalListLength+=table.get(i).size();
+		}
+		return totalListLength/table.size();
+	}
+	
+	//helper method to rehash
+	private void reHash() {
+		ArrayList<LinkedList<MapEntry<K, V>>> tempTable = new ArrayList<LinkedList<MapEntry<K, V>>>();
+		for(int i = 0; i < table.size()*2; i++) {
+			tempTable.add(new LinkedList<MapEntry<K, V>>());
+			
+		//don't know where to go from here 
+		}
 	}
 	
 	
